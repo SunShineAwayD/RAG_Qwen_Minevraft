9c1c0a6830b08fe02e44144d624ea3b06cede399
孙甲兴（sunjiaxing） <49228626+SunShineAwayD@users.noreply.github.com>
2025-08-31 22:40:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,14 @@
-# config.py
-
 import torch
 from llama_index.core import Settings
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-# --- 路径配置 ---
-# 提醒：请确保此路径是正确的。根据记录，您常用的路径是 '/mnt/data/SFT/models'。
+
 LLM_MODEL_PATH = "/media/sdb1/sjx/Qwen2.5-7B-Instruct"
 EMBED_MODEL_PATH = "/media/sdb1/sjx/bge-base-en-v1.5"
 JSON_DATA_PATH = "/media/sdb1/sjx/ragMinecraft/minecraft_qa.json"
-INDEX_SAVE_PATH = "/media/sdb1/sjx/ragMinecraft/saved_index" # 索引保存路径
-# --- 模型相关函数 ---
+INDEX_SAVE_PATH = "/media/sdb1/sjx/ragMinecraft/saved_index" 
 
-# 定义 Qwen2 的 prompt 模板
 def messages_to_prompt(messages):
     prompt = ""
     for message in messages:
@@ -30,9 +25,7 @@ def messages_to_prompt(messages):
 def completion_to_prompt(completion):
      return f"<|im_start|>assistant\n{completion}<|im_end|>\n"
 
-# 封装模型设置的函数
 def setup_models():
-    """初始化并配置LLM和Embedding模型"""
     print("开始配置模型...")
     
     device = "cuda" if torch.cuda.is_available() else "cpu"
